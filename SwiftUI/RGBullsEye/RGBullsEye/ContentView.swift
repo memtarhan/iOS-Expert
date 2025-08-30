50105f1852ee618ee50a5c0e952ebb140cfc96cf
Mehmet Tarhan <hi@memtarhan.com>
2025-08-30 21:58:37 +0100

    <SwiftUI>: Tutorials - Getting started with SwiftUI

--- SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,12 +33,72 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, world!")
-      .padding()
-  }
+    @State var game = Game()
+    @State var guess: RGB
+    var target = RGB.random()
+
+    @State var showScore = false
+
+    var body: some View {
+        VStack {
+            ColorCircle(rgb: $game.target)
+            if !showScore {
+                Text("R: ??? G: ??? B: ???")
+                    .padding()
+            } else {
+                Text(game.target.intString())
+                    .padding()
+            }
+            ColorCircle(rgb: $guess)
+            Text(guess.intString())
+                .padding()
+            ColorSlider(value: $guess.red, trackColor: .red)
+            ColorSlider(value: $guess.green, trackColor: .green)
+            ColorSlider(value: $guess.blue, trackColor: .blue)
+
+            Button {
+                showScore = true
+                game.check(guess: guess)
+            } label: {
+                Text("Hit Me!")
+            }
+        }
+        .alert(isPresented: $showScore) {
+            Alert(
+                title: Text("Your Score"),
+                message: Text(String(game.scoreRound)),
+                dismissButton: .default(Text("OK")) {
+                    game.startNewRound()
+                    guess = RGB()
+                })
+        }
+    }
 }
 
 #Preview {
-    ContentView()
+    ContentView(guess: RGB(red: 0.8, green: 0.3, blue: 0.7))
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var trackColor: Color
+
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value)
+                .accentColor(trackColor)
+            Text("255")
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct ColorCircle: View {
+    @Binding var rgb: RGB
+
+    var body: some View {
+        Circle()
+            .fill(Color(rgbStruct: rgb))
+    }
 }
