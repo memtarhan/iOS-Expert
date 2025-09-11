7ffc6a61e94d7a5cdba0e15b33e3249921f97c63
Mehmet Tarhan <hi@memtarhan.com>
2025-09-11 12:55:14 +0100

    Lecture 4 | Stanford CS193p 2023 - Applying MVVM

--- stanford-cs193p-2023/Memorize/Memorize/EmojiMemoryGame.swift
+++ stanford-cs193p-2023/Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,6 +7,33 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemoryGame<String>
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🕷️", "🐥", "🐝", "🐒", "🦆", "🐞", "🦉", "🐸"]
+
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+                
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+
+    @Published private var model = createMemoryGame()
+
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
 }
